7c432e6f6e1e1f07a1d72dd872d3c61ec3417660
jayasimha4226 <127817990+jayasimha4226@users.noreply.github.com>
2023-11-04 21:15:07 +0530

    Create firebase.js

---
+++ src/components/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export const auth = firebase. initialization({
+    
+        apiKey: "AIzaSyAwaUxua8L2wAsqj_HLHfCVBP7EGRRFnx8",
+        authDomain: "unichat-4dac7.firebaseapp.com",
+        projectId: "unichat-4dac7",
+        storageBucket: "unichat-4dac7.appspot.com",
+        messagingSenderId: "1094564510873",
+        appId: "1:1094564510873:web:44ff1d5525acb7e4e1cc08"
+      
+}).auth();
